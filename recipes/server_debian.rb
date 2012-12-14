b4145c1b373451fbc1f6212d68453023e93c1927
hpathan <harunap@gmail.com>
2012-12-14 14:18:57 +0530

    added utf8 support before installing on ubuntu platform

--- recipes/server_debian.rb
+++ recipes/server_debian.rb
@@ -19,6 +19,10 @@
 # limitations under the License.
 #
 
+ENV['LANGUAGE'] = 'en_US.UTF-8'
+ENV['LANG'] = 'en_US.UTF-8'
+ENV['LC_ALL'] = 'en_US.UTF-8'
+
 include_recipe "postgresql::client"
 
 case node[:postgresql][:version]
